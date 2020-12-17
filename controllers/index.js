1388a220a32662c24417f4bb1387e2e9b2868059
victordoyle <victor-doyle@outlook.com>
2020-12-16 21:10:03 -0500

    merged test branch into main victor branch

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,7 @@
 module.exports = {
-<<<<<<< HEAD
     users: require("./users"),
     /* posts: require("./posts"), //Uncomment after testing
     images: require("./images"),
     comments: require("./comments") */
   }
-=======
-    users: require("./authors"),
-    posts: require("./articles"),
-    images: require("./images"),
-    comments: require("./comments")
-  }
->>>>>>> victor
+tor
