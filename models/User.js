16580d2105e495a9133fb671915772267e0f40d6
victordoyle <victor-doyle@outlook.com>
2020-12-16 23:07:34 -0500

    added required:true on User models

--- models/User.js
+++ models/User.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const UsersSchema = new mongoose.Schema(
     {
-        firstName: { type: String },
-        lastName: { type: String },
-        userName: { type: String },
-        description: { type: String },
+        firstName: { type: String, required: [true, "You must provide a first name"]},
+        lastName: { type: String, required: [true, "You must provide a last name"] },
+        userName: { type: String, required: [true, "You must provide a username"] },
+        description: { type: String, required: [true, "You must provide a profile bio"] },
         posts: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
         picture: { type: mongoose.Schema.Types.ObjectId, ref: "Image" },
         savedPosts: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
