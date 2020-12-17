59fe56e7d426d5f7c65d4229007f1be792a4f532
platothegeek <platothegeek@gmail.com>
2020-12-16 16:40:06 -0800

    Editted and flushed out models/index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoURL = "mongodb://localhost27017/Users"
+mongoURL = "mongodb://localhost27017/Insta"
 
 mongoose.connect(mongoURL, {
     //This following part of code I was confused about and just copied directly from express fruits
@@ -22,5 +22,8 @@ mongoose.connection.on("connected", function(){
     console.log("SCPiNet error", error);
   });
   module.exports = {
-      User: require("./users")
+      User: require("./users"),
+      Post: require("./posts"),
+      Image: require("./images"),
+      Comment: require("./comments"),
   }
