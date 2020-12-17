c9e2582d11616089eb14b5e02acadfab198bf036
platothegeek <platothegeek@gmail.com>
2020-12-16 16:35:51 -0800

    Added index.js basics

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+mongoURL = "mongodb://localhost27017/Users"
+
+mongoose.connect(mongoURL, {
+    //This following part of code I was confused about and just copied directly from express fruits
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+})
+
+mongoose.connection.on("connected", function(){
+    console.log("SCPiNet Connected");
+  });
+  
+  mongoose.connection.on("disconnected", function(){
+    console.log("SCPiNet disconnected");
+  });
+  
+  mongoose.connection.on("error", function(error){
+    console.log("SCPiNet error", error);
+  });
+  module.exports = {
+      User: require("./users")
+  }
