ad6c88862c47141a7ba3729aa1d6f01a64b556a2
platothegeek <platothegeek@gmail.com>
2020-12-18 10:08:44 -0800

    Added edit route and view

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,6 +4,17 @@ const express = require("express");
 const router = express.Router();
 // database setup
 const db = require('../models');
+// Rest Routes
+/* 
+  * Index - GET - /users  - Presentational - all users
+  * New - GET - /users/new  - Presentational Form - user signup page
+  * Show - GET - /users/:id  - Presentational - show your user page or other user profiles
+  * Create - Post - /users  - Functional - data receiving for route to create new user
+  * Edit - GET - /users/:id/edit  - Presentational Form - edit user profile with previous filled data
+  * Update - PUT - /users/:id  - Functional - update user profile with new changes
+  * Delete - DELETE - /users/:id  - Functional - Deletes user profile by id from request (FIXME: Make sure to make delete only available to your own profile)
+*/
+
 /* ======== INDEX PAGE ======== */
 router.get("/posts", function(request, response){
     db.Post.find({}, function(error, allPosts){
@@ -33,6 +44,17 @@ router.post("/upload", function(request,response){
                 
             return response.redirect("/home");
         })
+    });
+});
+//Edit route
+router.get("/:id/edit", function(request,response){
+    db.Post.findById(request.params.id, function(error, foundUser){
+        if(error) {
+            return response.render(error);
+        } else {
+            const context = {users: foundUser};
+            return response.render("posts/edit", context);
+        }
     });
 });
 //Delete route
