0e27b75665a0988ebe7a888ae0d2a1abbda94d54
platothegeek <platothegeek@gmail.com>
2020-12-17 16:31:05 -0800

    Finished new post route and editted Image schema

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,10 +8,19 @@ const db = require('../models');
 // New Post route
 router.post("/post/upload", function(request,response){
     db.Post.create(request.body, function(error, createdPost){
-        if(error) {
-            return response.send(error);
-        } else {
-            return response.redirect("/users");
-        }
+        if (error) return response.send(error);
+        db.User.findById(createdPost.user).exec(function(error, foundUser){
+            foundUser.posts.push(createdPost);
+            foundUser.save();
+        })
+    });
+    db.Image.create(request.body, function(error, createdImage){
+        if (error) return response.send(error);
+        db.Post.findById(createdImage.post).exec(function(error, foundPost){
+            foundPost.image.push(createdImage);
+            foundImage.save();
+                
+            return response.redirect("/home");
+        })
     });
 });
--- models/Image.js
+++ models/Image.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ImageSchema = new mongoose.Schema(
     {
-        user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}, //Used for post/profile image assosciation (will help with versatility)
+        post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true}, //Used for post/profile image assosciation (will help with versatility)
         image: { data: Buffer, type: String}, //In this case the string is the image path, I am storing the image in Buffer for the usability since the image is stored in binary. .
     }
 )
