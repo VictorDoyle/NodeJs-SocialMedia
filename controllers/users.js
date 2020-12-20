9730c85edf593f3a9adf20a82b282ceb71c727c7
victordoyle <victor-doyle@outlook.com>
2020-12-19 19:09:34 -0500

    universal styles for pages + revamped friends list/search ui

--- controllers/users.js
+++ controllers/users.js
@@ -99,18 +99,6 @@ router.put("/:id", function(request,response){
 });
 
 
-/* ======== SEARCH ROUTE ======= */
-
-/* router.get('/search/:userName', function(request, response) {
-    let searchedUser = request.params.title;
-    Product.find({title: title}, function (err, products) {
-        if(err) {
-            return res.render('shop/search', {products: null});
-            ^^^^^^
-        }
-        res.render('shop/search', {products: products});
-    });
-}); */
 
 
 
