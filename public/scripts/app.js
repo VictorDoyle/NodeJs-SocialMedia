e391d1aac1f25b7a2d8ef00bd85446ef30923a7b
victordoyle <victor-doyle@outlook.com>
2020-12-22 18:48:32 -0500

    precommit before testing user profike pics upload

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,6 +11,7 @@ console.log("sanity check")
 
 /*  Follow a user event listener */
 let followerTotal = 1;
+/* FIXME: add fetch to save follower value on db */
 
 $("#followButton").on("click", function followCountToggle(){ 
     $("#followerCount").text(`${followerTotal}`); 
@@ -28,8 +29,12 @@ $("#followButton").on("click", function followCountToggle(){
 });
 
 // SECTION: USER POSTS COMMENTS/LIKES
+/* like button id # */
+/* id="likeButton" */
+/* like value id */
+/*  post.likes */
 
-
+$("#likeButton").on("click", function addLikes() {});
 
 
 /* ========================== NOTIFICATIONS ========================== */
