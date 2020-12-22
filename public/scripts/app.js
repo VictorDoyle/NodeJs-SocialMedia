f0252fc34b1493b677a8966e1ca0e2460dfc7d88
victordoyle <victor-doyle@outlook.com>
2020-12-21 19:40:51 -0500

    added evt listeners to follow/unfollow users

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,32 @@
 console.log("sanity check")
 
-/* Post-like event listener */
+
+/* ========================== USER PROFILES ========================== */
+/* Follower profile number event listener */
+
+/* follower button id # */
+followButton
+/* follower value id */
+followerCount
+
+/*  Follow a user event listener */
+let followerTotal = 0;
+$("#followButton").on("click", function followCountAdd(){ 
+  followerTotal++; 
+    $("#followerCount").text(`${followerTotal}`); 
+    $("#followButton").off("click"); /* limit to follow once */
+    /* change follow button to "unfollow" */
+    $("#followButton").addClass("ui basic button");
+    $("#followButton").text("Unfollow");
+});
+
+/* Unfollow */
 
 
+/* ========================== NOTIFICATIONS ========================== */
 
 
-$(".left floated like").on( "click", function() {
-    console.log("button clicked");
-  });
+/* ========================== HOME FEED ========================== */
+
+
+/* ========================== SETTINGS ========================== */
